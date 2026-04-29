ecd302a564f565072961a0f4aa56657a5d61e4c7
Your Name <you@example.com>
2026-04-29 21:21:10 +0900

    add: src/app/robots.ts for dynamic robots.txt generation

---
+++ src/app/robots.ts
@@ -0,0 +1,19 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      { userAgent: 'Amazonbot', disallow: '/' },
+      { userAgent: 'Applebot-Extended', disallow: '/' },
+      { userAgent: 'Bytespider', disallow: '/' },
+      { userAgent: 'CCBot', disallow: '/' },
+      { userAgent: 'ClaudeBot', disallow: '/' },
+      { userAgent: 'CloudflareBrowserRenderingCrawler', disallow: '/' },
+      { userAgent: 'Google-Extended', disallow: '/' },
+      { userAgent: 'GPTBot', disallow: '/' },
+      { userAgent: 'meta-externalagent', disallow: '/' },
+      { userAgent: '*', allow: '/' },
+    ],
+    sitemap: 'https://nowtools.kr/sitemap.xml',
+  };
+}
